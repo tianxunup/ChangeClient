9f72197ff115085f9d4d0fc46940d661b1413d36
GitTIanxun163 <tianxunwx@163.com>
2026-07-26 13:31:50 +0800

    代码优化

--- src/main/java/cc/tianxun/changeclient/mixin/ClientPacketListenerMixin.java
+++ src/main/java/cc/tianxun/changeclient/mixin/ClientPacketListenerMixin.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin{
+	@Unique
 	private static long last = -1;
 
 	@Inject(method = "handleSetTime", at = @At("RETURN"))
